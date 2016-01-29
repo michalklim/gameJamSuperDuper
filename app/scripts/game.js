96124ded984deb327af2a560ced87729c0d86707
Michal Klim <michal.wklim@gmail.com>
2016-01-29 21:58:24 +0100

    Canvas dimensions = window dimensions

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,5 +1,5 @@
 import * as states from './states';
-const GAME = new Phaser.Game(800, 1000, Phaser.AUTO);
+const GAME = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO);
 
 Object.keys(states).forEach(state => GAME.state.add(state, states[state]));
 
