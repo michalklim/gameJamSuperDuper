b273196a9f8034915e802cc3cdaed6cc84a6693b
Michal Klim <michal.wklim@gmail.com>
2016-01-31 12:39:00 +0100

    Add A D key support

--- app/scripts/prefabs/planet.js
+++ app/scripts/prefabs/planet.js
@@ -13,21 +13,26 @@ export default class Planet extends Phaser.Sprite {
     this.scale.setTo(this.targetDim / this.texture.width);
     this.anchor.setTo(0.5);
     this.game.physics.arcade.enable(this);
+
+    //Planet keyboard steering
     this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    this.AKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+    this.DKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
 
+    //Planet mousewheel steering
     this.game.input.mouse.mouseWheelCallback = (e) => {
-      var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+      let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
       this.rotation += delta * 0.15;
     };
   }
 
   update() {
-    if (this.leftKey.isDown)
+    if (this.leftKey.isDown || this.AKey.isDown)
     {
       this.rotation -= 0.04;
     }
-    else if (this.rightKey.isDown)
+    else if (this.rightKey.isDown || this.DKey.isDown)
     {
       this.rotation += 0.04;
     }
